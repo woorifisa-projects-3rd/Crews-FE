29fe51926d40a0e26838d21cb9912d053ae56f56
bw02184 <bw02184@naver.com>
2024-11-20 12:31:23 +0900

    fix: 필요없는 파일 삭제

---
+++ app/service/agits/[id]/accounts/create/complete/page.jsx
@@ -0,0 +1,27 @@
+import { Header } from '@/components/common';
+import { Flex } from '@radix-ui/themes';
+
+import { products } from '@/constants/dummy';
+import DepositProduct from '@/components/agits/Account/AccountCreate';
+
+export default async function Page() {
+  return (
+    <div className="page">
+      <header>
+      <Text as="p" size="1" weight="medium">
+
+      </header>
+      <Flex direction="column" gap="10px" className="content">
+        <section>
+          <Flex direction="column" gap="10px" asChild>
+            <ul>
+              {products.map((product, index) => {
+                return <DepositProduct key={index} product={product}></DepositProduct>;
+              })}
+            </ul>
+          </Flex>
+        </section>
+      </Flex>
+    </div>
+  );
+}
